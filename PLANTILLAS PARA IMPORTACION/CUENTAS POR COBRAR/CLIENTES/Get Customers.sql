32d312ed29dee7a0c0605ebf82fc36d085c1aefe
ivanporras <ivanporraslopez@gmail.com>
2017-12-16 14:14:14 -0600

    Ultima version

--- PLANTILLAS PARA IMPORTACION/CUENTAS POR COBRAR/CLIENTES/Get Customers.sql
+++ PLANTILLAS PARA IMPORTACION/CUENTAS POR COBRAR/CLIENTES/Get Customers.sql
@@ -17,12 +17,13 @@ select top 1000
 
 
 -- OBTENER BALANZAZ DE COMPROBACION DE CLIENTES
-
 use SLAvicapp
-
-select  aracct,arsub,CustId,CName,sum(DocBal)/2 'SUMA' 
-		from vr_08621
-group by aracct,arsub,CustId,CName, GrpAcctSub
+select  vr.aracct,vr.arsub,vr.CustId,CName,customer.Addr1,customer.BillZip,sum(DocBal)/2 'SUMA' 
+		from vr_08621 vr
+		inner join customer customer on customer.Name = vr.CName
+		
+group by vr.aracct,vr.arsub,vr.CustId,CName, GrpAcctSub,customer.Addr1,customer.BillZip
+having sum(DocBal) <> 0
 order by arsub asc
 	
 	
